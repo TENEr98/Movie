6598dbb761a06f8e16ad20f6954a49e34dd667f2
TENEr98 <levon.tarayan.98@mail.ru>
2022-02-08 17:53:31 +0500

    refactor: remove console

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, memo } from 'react'
 import { Icon, IconButton, Loader } from 'rsuite'
 import { MovieAPI } from '../api'
 import { notify } from '../utils/Notification'
@@ -141,4 +141,4 @@ const MoveDetails = () => {
   )
 }
 
-export default MoveDetails
+export default memo(MoveDetails)
